301dc65fe2853e72a840657c402a7f7fee0f1fbf
Flavius <highstrike@gmail.com>
2020-02-28 21:23:11 +0200

    Update db.sql

--- db.sql
+++ db.sql
@@ -1,19 +1,14 @@
---
--- Table structure for table `live_streams`
---
-
-DROP TABLE IF EXISTS `live_streams`;
-CREATE TABLE IF NOT EXISTS `live_streams` (
+CREATE TABLE `streamers` (
   `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT,
   `user_id` bigint(20) UNSIGNED NOT NULL,
   `stream_id` bigint(20) UNSIGNED NOT NULL,
-  `title` varchar(256) NOT NULL,
-  `name` varchar(64) NOT NULL,
-  `url` varchar(128) NOT NULL,
-  `thumbnail` varchar(192) NOT NULL,
+  `title` varchar(256) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `name` varchar(64) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `url` varchar(128) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `thumbnail` varchar(192) COLLATE utf8mb4_unicode_ci NOT NULL,
   `viewers` int(11) NOT NULL,
   `started_at` datetime NOT NULL,
   `created_at` datetime NOT NULL,
   PRIMARY KEY (`id`),
   UNIQUE KEY `user_id` (`user_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
